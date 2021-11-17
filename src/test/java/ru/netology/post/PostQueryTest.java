9319cbe6b2c0fb23082ccc7d511e2327a5601230
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-17 21:16:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/post/PostQueryTest.java
@@ -0,0 +1,23 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class PostQueryTest {
+
+    @Test
+    void shouldReturnAnswer() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
